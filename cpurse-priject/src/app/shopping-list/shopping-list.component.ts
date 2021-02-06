1ab9be11f9f06b19c26edc243f10b612c2c47ec8
Milan Lacok <milan.lacok@navigator.rs>
2021-02-06 23:37:04 +0100

    Shopping list update and Ingredient model added

--- cpurse-priject/src/app/shopping-list/shopping-list.component.ts
+++ cpurse-priject/src/app/shopping-list/shopping-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 @Component({
   selector: 'app-shopping-list',
@@ -6,7 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./shopping-list.component.css']
 })
 export class ShoppingListComponent implements OnInit {
-
+  ingredients: Ingredient[] = [
+    new Ingredient('apples', 5),
+    new Ingredient('pears', 2),
+    new Ingredient('water', 7)
+  ];
   constructor() { }
 
   ngOnInit(): void {
