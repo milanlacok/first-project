399328dce5e58535b760aee7108cbb6f1b1c4e8a
Milan Lacok <milan.lacok@navigator.rs>
2021-02-06 23:10:17 +0100

    Added recipe to the form

--- cpurse-priject/src/app/recipes/recipe-list/recipe-list.component.ts
+++ cpurse-priject/src/app/recipes/recipe-list/recipe-list.component.ts
@@ -7,7 +7,10 @@ import { Recipe } from '../recipe.model';
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-  recipes: Recipe[] = [];
+  recipes: Recipe[] = [
+    new Recipe('Steak', 'Test steak', 'https://i.pinimg.com/564x/b0/a5/4b/b0a54b26b6545c87a07a07aa5a311f8e.jpg'),
+    new Recipe('Lasagne', 'Test lasagne', 'https://i.pinimg.com/originals/08/ca/24/08ca240166714b12ccdc074ffaccc40f.jpg')
+  ];
   constructor() { }
 
   ngOnInit(): void {
